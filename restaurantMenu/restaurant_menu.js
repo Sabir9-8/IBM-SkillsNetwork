7ee56f83f6e9318fc6c219dee4b3c87a49d40091
Md Sabir Ahamed <mdahamedsabir98@gmail.com>
2026-06-02 15:41:36 +0530

    restaurant menu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,19 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItems = breakfastMenu.map((item, index) => `<p> Item ${index + 1}: ${item}</p>`).join("");
+document.getElementById("breakfastMenuItems").innerHTML = breakfastMenuItems;
+
+let mainCourseItems = "";
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItems += `<p>Item ${index + 1}: ${item}</p>`;
+});
+
+document.getElementById("maincourseMenuItems").innerHTML = mainCourseItems;
+
+let dessertItem = "";
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById("dessertMenuItems").innerHTML = dessertItem;
