889b34abd2efc9693206e99022bfa18dc50ae287
Md Sabir Ahamed <mdahamedsabir98@gmail.com>
2026-06-08 20:40:32 +0530

    adding ui and logic to speedAnalysis

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -11,8 +11,24 @@ function startTest() {
 
     document.getElementById("output").innerHTML = "";
     startTime = new Date().getTime();
+    //console.log(startTime);
 }
 
 function endTest() {
     endTime = new Date().getTime();
+    //console.log(endTime);
+    const userInput = document.getElementById("userInput");
+    userInput.readOnly = true;
+    let timeElapsed = (endTime - startTime) / 1000;
+    let userText = userInput.value.trim();
+    let typeWords = userText === "" ? 0 : userText.split(/\s+/).length;
+    let wpm = 0;
+    if (timeElapsed !== 0) {
+        wpm = Math.round((typeWords / timeElapsed) * 60);
+    }
+    document.getElementById("output").innerHTML = 
+    `<h2>Typing test Result:</h2>
+    <p>Words Typed: ${typeWords}</p>
+    <p>Time Ellapsed: ${timeElapsed.toFixed(2)}sec
+    <p>Words per minute (WPM): ${wpm}</p>`;
 }
