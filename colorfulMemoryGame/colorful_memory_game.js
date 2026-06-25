823c4e6adf59a964c844b393b167c9495676e049
Md Sabir Ahamed <mdahamedsabir98@gmail.com>
2026-06-25 22:14:42 +0530

    full colorful memory game

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -1,7 +1,5 @@
 const colors = ['red', 'blue', 'green', 'purple', 'orange', 'pink', 'red', 'blue', 'green', 'purple', 'orange', 'pink'];
-let cards = shuffle(colors.concat(colors));
-//console.log(cards);
-
+let cards = [];
 let selectedCards = [];
 let score = 0;
 let timeLeft = 30;
@@ -24,9 +22,71 @@ function generateCards() {
 
 function shuffle(array) {
     for (let i = 0; i < array.length; i++) {
-        const j = Math.floor(Math.random() * (i+1));
+        const j = Math.round(Math.random() * (i+1));
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
 }
 
+function checkMatch() {
+    const [card1, card2] = selectedCards;
+    if (card1.dataset.color == card2.dataset.color) {
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        score += 2;
+        scoreElement.textContent = `Score: ${score}`;
+    }
+    else {
+        card1.textContent = '?';
+        card2.textContent = '?';
+        card1.style.backgroundColor = '#ddd';
+        card2.style.backgroundColor = '#ddd';
+    }
+    selectedCards = [];
+}
+
+function handleCardClick(event) {
+    const card = event.target;
+    if (!card.classList.contains('card') || card.classList.contains('matched')) {
+        return;
+    }
+    card.textContent = card.dataset.color;
+    card.style.backgroundColor = card.dataset.color;
+    selectedCards.push(card);
+    if (selectedCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function startGame() {
+    let timeLeft = 30;
+    startbtn.disabled = true;
+    score = 0;
+    scoreElement.textContent = `Score: ${score}`;
+    startGameTimer(timeLeft);
+    cards = shuffle(colors.concat(colors));
+    selectedCards = [];
+    gameContainer.innerHTML = "";
+    generateCards();
+    gameContainer.addEventListener("click", handleCardClick);
+}
+
+function startGameTimer(timeLeft) {
+    timerElement.textContent = `Time Left: ${timeLeft}`;
+    gameInterval = setInterval(() => {
+        timeLeft--;
+        timerElement.textContent = `Time Left: ${timeLeft}`;
+
+        if (timeLeft == 0) {
+            clearInterval(gameInterval);
+            alert("Game Over!");
+            startbtn.disabled = false;
+            setTimeout(() => {
+                gameContainer.innerHTML = "";
+                scoreElement.textContent = `Score: 0`;
+            }, 4000)
+        }
+    }, 1000);
+}
+
+startbtn.addEventListener("click", startGame);
